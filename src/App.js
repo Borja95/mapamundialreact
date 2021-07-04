978b5e3d1122f66417362980bb16fb3270edb2e3
Arturo Borja <abj951122@gmail.com>
2021-07-04 12:02:47 -0500

    version 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ReactWorldCountriesMap } from "react-world-countries-map"
 
 function App() {
+  const data =
+  [
+    { country: "us", value: 22675271 },  // Estados Unidos
+    { country: "cn", value: 16642318 }, // China
+    { country: "jp", value: 5378136 }, // Japón
+    { country: "de", value:  4319286},  // Alemania
+    { country: "gb", value: 3124650 },  // Reino Unido
+    { country: "in", value: 3049704 }, // india
+    { country: "fr", value: 2938271 },  // francia
+    { country: "it", value: 2106287 },  // italia
+    { country: "ca", value: 1883487 },  // canada
+    { country: "kr", value:  1806707},  // corea del sur
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <ReactWorldCountriesMap color="blue"
+     tooltipBgColor="blue" tooltipTextColor="white"
+     title="Top 10 Países con mayor PIB según el FMI" value-suffix="MDD" value-prefix="$" size="responsive" data={data} />
     </div>
   );
 }
